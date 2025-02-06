ab124ea30ae3d2b77e36a15327f81be6cd79ad48
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2025-02-06 11:47:27 +0800

    优化缓动拟合

--- RhythmBaseCS/Components/Easing/EaseValue.cs
+++ RhythmBaseCS/Components/Easing/EaseValue.cs
@@ -39,12 +39,10 @@ namespace RhythmBase.Components.Easing
 	public class EaseValue(float originalValue, IEnumerable<EaseNode> nodes)
 	{
 		public EaseNode[] Nodes { get; set; } = [.. nodes.OrderBy(x => x.Time)];
-
 		/// <summary>
 		/// Gets or sets the original value before any easing is applied.
 		/// </summary>
 		public float OriginalValue { get; set; } = originalValue;
-
 		/// <summary>
 		/// Gets the value at the specified time, taking into account the easing nodes.
 		/// </summary>
@@ -80,7 +78,6 @@ namespace RhythmBase.Components.Easing
 			origin = GetValue(ns.Pop(), time, origin);
 			return origin;
 		}
-
 		/// <summary>
 		/// Determines whether the specified time is within the range of the easing node.
 		/// </summary>
@@ -88,7 +85,6 @@ namespace RhythmBase.Components.Easing
 		/// <param name="time">The time to check.</param>
 		/// <returns><c>true</c> if the time is within the range of the easing node; otherwise, <c>false</c>.</returns>
 		private static bool IsInRange(EaseNode node, float time) => node.Time <= time && time <= node.Time + node.Duration;
-
 		/// <summary>
 		/// Calculates the value of the easing node at the specified time.
 		/// </summary>
@@ -97,7 +93,6 @@ namespace RhythmBase.Components.Easing
 		/// <param name="origin">The original value before any easing is applied.</param>
 		/// <returns>The calculated value at the specified time.</returns>
 		private static float GetValue(EaseNode node, float time, float origin) => node.Duration == 0 ? node.Target : (float)node.Type.Calculate((time - node.Time) / node.Duration, origin, node.Target);
-
 		/// <summary>
 		/// Fits the data points to an easing function with the specified precision.
 		/// </summary>
@@ -105,7 +100,16 @@ namespace RhythmBase.Components.Easing
 		/// <param name="values">The array of time and value pairs to fit.</param>
 		/// <param name="precision">The precision for fitting the data points.</param>
 		/// <returns>An <see cref="EaseValue"/> instance that fits the data points.</returns>
-		public static EaseValue Fit(float originalValue, (float time, float value)[] values, float precision = 0.1f)
+		public static EaseValue Fit(float originalValue, (float time, float value)[] values, float precision = 3f) => Fit(originalValue, values, eases, precision);
+		/// <summary>
+		/// Fits the data points to an easing function with the specified precision.
+		/// </summary>
+		/// <param name="originalValue">The original value before any easing is applied.</param>
+		/// <param name="values">The array of time and value pairs to fit.</param>
+		/// <param name="precision">The precision for fitting the data points.</param>
+		/// <param name="easeTypes">The array of easing types to consider.</param>
+		/// <returns>An <see cref="EaseValue"/> instance that fits the data points.</returns>
+		public static EaseValue Fit(float originalValue, (float time, float value)[] values, EaseType[] easeTypes, float precision = 3f)
 		{
 			values = [.. values.OrderBy(x => x.time)];
 			if (values.Length == 0)
@@ -114,36 +118,42 @@ namespace RhythmBase.Components.Easing
 				return new EaseValue(originalValue, [new EaseNode(values[0].value) { Duration = values[0].time, Type = EaseType.InOutSine }]);
 			if (values.Length == 2)
 				return new EaseValue(originalValue, [new EaseNode(values[0].value) { Time = values[0].time, Duration = values[1].time, Type = EaseType.InOutSine }]);
-			return Fit([(0, originalValue), .. values], precision);
+			return Fit([(0, originalValue), .. values], easeTypes, precision);
 		}
-
 		/// <summary>
 		/// Fits the data points to an easing function with the specified precision.
 		/// </summary>
 		/// <param name="values">The array of time and value pairs to fit.</param>
 		/// <param name="precision">The precision for fitting the data points.</param>
 		/// <returns>An <see cref="EaseValue"/> instance that fits the data points.</returns>
-		public static EaseValue Fit((float time, float value)[] values, float precision = 0.1f) => Fit(values, eases, precision);
+		public static EaseValue Fit((float time, float value)[] values, float precision = 3f) => Fit(values, eases, precision);
 		/// <summary>
 		/// Fits the data points to an easing function with the specified precision and ease types.
 		/// </summary>
 		/// <param name="values">The array of time and value pairs to fit.</param>
 		/// <param name="easeTypes">The array of easing types to consider.</param>
 		/// <param name="precision">The precision for fitting the data points.</param>
-		/// <remarks>Special thanks to mfgujhgh for the algorithm!</remarks>
+		/// <remarks>Special thanks to <b>mfgujhgh</b> for the algorithm!</remarks>
 		/// <returns>An <see cref="EaseValue"/> instance that fits the data points.</returns>
-		public static EaseValue Fit((float time, float value)[] values, EaseType[] easeTypes, float precision = 0.1f)
+		public static EaseValue Fit((float time, float value)[] values, EaseType[] easeTypes, float precision = 3f)
 		{
+			if (easeTypes.Length == 0)
+				throw new ArgumentException("At least one ease type must be specified.", nameof(easeTypes));
 			values = [.. values.OrderBy(x => x.time)];
-			int[] steps = new int[values.Length], p = new int[values.Length];
+			int[] steps = new int[values.Length],
+				p = new int[values.Length];
 			EaseType[] pe = new EaseType[values.Length];
 			steps[0] = 0;
 			for (int i = 1; i < values.Length; i++)
 			{
-				steps[i] = int.MaxValue;
-				for (int j = 0; j < i; j++)
+				steps[i] = steps[i - 1] + 1;
+				p[i] = i - 1;
+				pe[i] = easeTypes[0];
+				for (int j = 0; j < i - 1; j++)
 				{
-					EaseType besttype = EaseType.Linear;
+					if (steps[j] + 1 >= steps[i])
+						continue;
+					EaseType besttype = easeTypes[0];
 					float bests = float.MaxValue;
 					foreach (var e in easeTypes)
 					{
@@ -176,7 +186,6 @@ namespace RhythmBase.Components.Easing
 			}
 			return new EaseValue(values[0].value, ns);
 		}
-		private static readonly EaseType[] eases = Enum.GetValues<EaseType>();
 		private static float Check((float time, float value)[] data, int start, int end, EaseType type, float precision)
 		{
 			float s = 0;
@@ -193,5 +202,6 @@ namespace RhythmBase.Components.Easing
 			}
 			return s;
 		}
+		private static readonly EaseType[] eases = Enum.GetValues<EaseType>();
 	}
 }
