d45ed8b0f9edc0a33f4362cb6cf288f836132aab
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2025-05-06 12:00:21 +0800

    添加对事件栏位的限定

--- RhythmBaseCS/Events/WindowResize.cs
+++ RhythmBaseCS/Events/WindowResize.cs
@@ -8,17 +8,28 @@ using System.Threading.Tasks;
 
 namespace RhythmBase.Events
 {
+	/// <summary>  
+	/// Represents a window resize event.  
+	/// </summary>
 	public class WindowResize : BaseEvent, IEaseEvent
 	{
 		/// <inheritdoc/>
-		public override EventType Type => throw new NotImplementedException();
+		public override EventType Type => EventType.WindowResize;
 		/// <inheritdoc/>
-		public override Tabs Tab => throw new NotImplementedException();
+		public override Tabs Tab => CustomTab;
 		/// <summary>
 		/// Gets or sets the custom tab.
 		/// </summary>
 		[JsonProperty("tab")]
-		public Tabs CustomTab { get; set; }
+		public Tabs CustomTab
+		{
+			get; set
+			{
+				if (CustomTab is Tabs.Actions or Tabs.Windows)
+					field = value;
+				throw new InvalidOperationException();
+			}
+		}
 		/// <inheritdoc/>
 		public EaseType Ease { get; set; }
 		/// <inheritdoc/>
