738c048bf19d94ccf34385636386f97164bebea3
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2026-03-28 18:08:05 +0800

    移除 Angle  类型

--- RhythmBase/Adofai/Components/Angle.cs
+++ RhythmBase/Adofai/Components/Angle.cs
@@ -4,25 +4,25 @@ using System.Text;
 
 namespace RhythmBase.Adofai.Components
 {
-	public struct Angle
-	{
-		private int _round;
-		private float _value; // 96 = 360 / 3.75
-		private const float _unit = 3.75f;
-		private const float _full = 96f;
-		public readonly float Degree => _value * _unit + _round * 360f;
-		public readonly float Radian => (_value / _full + _round * 2) * float.Pi;
-		public static Angle FromDegree(float degree)
-		{
-			int round = (int)Math.Floor(degree / 360f);
-			float value = (degree - round * 360f) / 3.75f;
-			return new Angle { _round = round, _value = value };
+	//public struct Angle
+	//{
+	//	private int _round;
+	//	private float _value; // 96 = 360 / 3.75
+	//	private const float _unit = 3.75f;
+	//	private const float _full = 96f;
+	//	public readonly float Degree => _value * _unit + _round * 360f;
+	//	public readonly float Radian => (_value / _full + _round * 2) * float.Pi;
+	//	public static Angle FromDegree(float degree)
+	//	{
+	//		int round = (int)Math.Floor(degree / 360f);
+	//		float value = (degree - round * 360f) / 3.75f;
+	//		return new Angle { _round = round, _value = value };
 
-		}
-		public static Angle FromRadian(float radian)
-		{
-			float degree = radian / float.Pi * 180f;
-			return FromDegree(degree);
-		}
-	}
+	//	}
+	//	public static Angle FromRadian(float radian)
+	//	{
+	//		float degree = radian / float.Pi * 180f;
+	//		return FromDegree(degree);
+	//	}
+	//}
 }
