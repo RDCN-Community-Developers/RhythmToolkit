9f95ee98185736b02dff44b1298caf62538babc9
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2026-03-28 13:44:25 +0800

    统一轨道事件的 Y 值

--- RhythmBase/RhythmDoctor/Events/BaseBeat.cs
+++ RhythmBase/RhythmDoctor/Events/BaseBeat.cs
@@ -10,4 +10,12 @@ public abstract record class BaseBeat : BaseRowAction
 	/// Gets the tab associated with the beat action, which is always set to Rows.
 	/// </summary>
 	public override Tab Tab => Tab.Rows;
+	/// <summary>
+	/// Gets or sets the Y-coordinate of the object, inheriting the value from the parent if available; otherwise, uses the
+	/// base value.
+	/// </summary>
+	/// <remarks>When a parent object is present, this property reflects the parent's Y-coordinate, ensuring
+	/// positional consistency within a hierarchy. Setting this property directly updates the base value, which is used if
+	/// no parent is assigned.</remarks>
+    public override int Y { get => Parent?.Y ?? base.Y; set => base.Y = value; }
 }
