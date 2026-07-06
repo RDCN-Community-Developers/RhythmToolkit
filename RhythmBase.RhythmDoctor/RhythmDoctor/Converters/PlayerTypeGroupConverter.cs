f645a6528b7822d2418901f545e787bfc71dd92e
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2026-07-07 02:14:44 +0800

    修复严格模式下定长数组元素未被消费的异常

--- RhythmBase.RhythmDoctor/RhythmDoctor/Converters/PlayerTypeGroupConverter.cs
+++ RhythmBase.RhythmDoctor/RhythmDoctor/Converters/PlayerTypeGroupConverter.cs
@@ -10,21 +10,21 @@ internal class PlayerTypeGroupConverter : MetadataJsonConverter<PlayerTypeGroup>
 	public override PlayerTypeGroup Read(ref Utf8JsonReader reader, Type typeToConvert, MetadataJsonSerializerOptions options)
 	{
 		JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.StartArray);
+		reader.Read();
 		PlayerTypeGroup group = new();
 		if (options.Strictness == JsonStrictness.Strict)
 		{
 			for (int i = 0; i < 16; i++)
 			{
-				reader.Read();
 				JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.String);
-				group[i] = RhythmBase.Global.Converters.EnumConverter.TryParse(ref reader, out PlayerType type) ? type : PlayerType.NoChange;
+				group[i] = RhythmBase.Global.Converters.EnumConverter.TryParse(ref reader, out PlayerType type) && reader.Read() ? type : PlayerType.NoChange;
 			}
 			JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.EndArray);
 		}
 		else
 		{
 			int i = 0;
-			while (reader.Read() && reader.TokenType != JsonTokenType.EndArray)
+			while (reader.Read() && reader.TokenType != JsonTokenType.EndArray && i < 16)
 			{
 				JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.String);
 				group[i++] = RhythmBase.Global.Converters.EnumConverter.TryParse(ref reader, out PlayerType type) ? type : PlayerType.NoChange;
