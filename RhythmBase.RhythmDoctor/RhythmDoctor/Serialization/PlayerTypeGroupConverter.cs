f29e82da751caa3fc66ba673300e1ed72cf7b28f
OLDREDSTONE <OLDRedstone@users.noreply.github.com>
2026-08-22 19:58:03 +0800

    修复 '}' 读取异常

--- RhythmBase.RhythmDoctor/RhythmDoctor/Serialization/PlayerTypeGroupConverter.cs
+++ RhythmBase.RhythmDoctor/RhythmDoctor/Serialization/PlayerTypeGroupConverter.cs
@@ -10,26 +10,15 @@ internal class PlayerTypeGroupConverter : MetadataJsonConverter<PlayerTypeGroup>
 	public override PlayerTypeGroup Read(ref Utf8JsonReader reader, Type typeToConvert, MetadataJsonSerializerOptions options)
 	{
 		JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.StartArray);
-		reader.Read();
 		PlayerTypeGroup group = new();
-		if (options.Strictness == JsonStrictness.Strict)
+		int i = 0;
+		while (reader.Read() && reader.TokenType != JsonTokenType.EndArray && i < 16)
 		{
-			for (int i = 0; i < 16; i++)
-			{
-				JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.String);
-				group[i] = RhythmBase.Global.Serialization.EnumConverter.TryParse(ref reader, out PlayerType type) && reader.Read() ? type : PlayerType.NoChange;
-			}
-			JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.EndArray);
+			JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.String);
+			group[i++] = RhythmBase.Global.Serialization.EnumConverter.TryParse(ref reader, out PlayerType type) ? type : PlayerType.NoChange;
 		}
-		else
-		{
-			int i = 0;
-			while (reader.Read() && reader.TokenType != JsonTokenType.EndArray && i < 16)
-			{
-				JsonException.ThrowIfNotMatch(ref reader, JsonTokenType.String);
-				group[i++] = RhythmBase.Global.Serialization.EnumConverter.TryParse(ref reader, out PlayerType type) ? type : PlayerType.NoChange;
-			}
-		}
+		while (reader.TokenType != JsonTokenType.EndArray)
+			reader.Read();
 		return group;
 	}
 
